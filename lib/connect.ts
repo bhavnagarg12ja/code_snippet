79318e18ff9e4d9e1e517bae3ef45b4fea01465f
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-18 00:04:57 +0530

    db connection

---
+++ lib/connect.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+async function connect(): Promise<void> {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log('db connected..');
+  } catch (error) {
+    console.error(error);
+  }
+}
