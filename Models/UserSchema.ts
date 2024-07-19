9b2f012d615407a389e97b0eab20eb10d900e1f2
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-19 07:55:03 +0530

    fixes

--- Models/UserSchema.ts
+++ Models/UserSchema.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema(
   {
-    clerkUserid: { type: String, unique: true, required: true },
+    clerkUserId: { type: String, unique: true, required: true },
     emailAddress: { type: String, required: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
+
 const User = mongoose.models.User || mongoose.model('User', userSchema);
+
 export default User;
